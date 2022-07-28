afa9dcbb2b5ed526e888845355ccbd135f43a02e
MaulingMonkey <git@maulingmonkey.com>
2022-07-27 17:59:37 -0700

    error::LastError: terser Debug

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,9 @@
 use winapi::um::errhandlingapi::GetLastError;
+use std::fmt::{self, Debug, Formatter};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct LastError(pub(crate) u32);
 
@@ -9,6 +12,12 @@ impl LastError {
     pub fn get() -> Self { Self(get_last_error()) }
 
     pub fn as_u32(self) -> u32 { self.0 }
+}
+
+impl Debug for LastError {
+    fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
+        write!(fmt, "LastError({})", self.0)
+    }
 }
 
 impl From<LastError> for u32 { fn from(err: LastError) -> Self { err.0 } }
