4960c93257c00cc9b719c0149d121726c9bab7c2
MaulingMonkey <git@maulingmonkey.com>
2022-07-29 17:25:28 -0700

    LsaLookupSids2

--- src/refs/sid.rs
+++ src/refs/sid.rs
@@ -1,6 +1,13 @@
+// https://docs.microsoft.com/en-us/windows/win32/api/ntsecapi/nf-ntsecapi-lsalookupsids
+
+use crate::From32;
+
 use abistr::CStrPtr;
 
+use winapi::shared::ntstatus::STATUS_SUCCESS;
 use winapi::shared::sddl::ConvertSidToStringSidA;
+use winapi::um::lsalookup::{LSA_OBJECT_ATTRIBUTES, LSA_REFERENCED_DOMAIN_LIST, LSA_TRANSLATED_NAME};
+use winapi::um::ntlsa::*;
 use winapi::um::winbase::LocalFree;
 use winapi::um::winnt::{SID, LPSTR, SID_AND_ATTRIBUTES};
 
@@ -24,11 +31,48 @@ impl SidPtr<'_> {
         assert!(succeeded, "ConvertSidToStringSidA");
         local_string
     }
+
+    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/ntsecapi/nf-ntsecapi-lsalookupsids2)\] LsaLookupSids2
+    pub fn lsa_lookup_sids2(&self) -> String {
+        // .cast() spam notes:
+        // it appears PLSA_HANDLE points to void, not LSA_HANDLE, for whatever twisted reason.
+        // it appears PSID points to void, not SID, for whatever twisted reason.
+
+        let system_name = null_mut();
+        let mut object_attributes = unsafe { std::mem::zeroed::<LSA_OBJECT_ATTRIBUTES>() };
+        object_attributes.Length = std::mem::size_of_val(&object_attributes) as _;
+        let desired_access = POLICY_LOOKUP_NAMES;
+        let mut policy = null_mut();
+        assert!(STATUS_SUCCESS == unsafe { LsaOpenPolicy(system_name, &mut object_attributes, desired_access, &mut policy) });
+        let policy = policy.cast(); // PSID -> *mut SID
+
+        let lookup_options = LSA_LOOKUP_DISALLOW_CONNECTED_ACCOUNT_INTERNET_SID; // consider also: LSA_LOOKUP_PREFER_INTERNET_NAMES ?
+        let mut domains = null_mut();
+        let mut names = null_mut();
+        assert!(STATUS_SUCCESS == unsafe { LsaLookupSids2(policy, lookup_options, 1, [self.0.cast()].as_mut_ptr(), &mut domains, &mut names) });
+
+        let result = {
+            let domains : &LSA_REFERENCED_DOMAIN_LIST = unsafe { &*domains };
+            let _domains = unsafe { std::slice::from_raw_parts(domains.Domains, usize::from32(domains.Entries)) };
+            let name : &LSA_TRANSLATED_NAME = unsafe { &*names };
+            let name_name = unsafe { std::slice::from_raw_parts(name.Name.Buffer, (name.Name.Length/2).into()) }; // Length is in *bytes*, Buffer is *wchar_t* s
+            //dbg!(domains.len());
+            //dbg!(name.DomainIndex);
+            //dbg!(name.Use);
+            String::from_utf16_lossy(name_name)
+        };
+
+        assert!(STATUS_SUCCESS == unsafe { LsaFreeMemory(domains.cast()) });
+        assert!(STATUS_SUCCESS == unsafe { LsaFreeMemory(names.cast()) });
+        assert!(STATUS_SUCCESS == unsafe { LsaClose(policy) });
+
+        return result;
+    }
 }
 
 impl<'a> Debug for SidPtr<'a> {
     fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
-        write!(fmt, "PSID({})", self.to_string_sid_a())
+        write!(fmt, "{} {:?}", self.to_string_sid_a(), self.lsa_lookup_sids2())
     }
 }
 
