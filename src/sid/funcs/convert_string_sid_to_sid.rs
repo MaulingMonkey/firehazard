adba5acf910de92dacf845e5192183e9685d2810
MaulingMonkey <git@maulingmonkey.com>
2022-08-06 14:24:51 -0700

    doc: ConvertStringSidToSid: improve 1-liners

--- src/sid/funcs/convert_string_sid_to_sid.rs
+++ src/sid/funcs/convert_string_sid_to_sid.rs
@@ -11,7 +11,8 @@ use std::ptr::null_mut;
 
 
 
-/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertstringsidtosida)\] ConvertStringSidToSidA
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertstringsidtosida)\]
+/// ConvertStringSidToSidA - convert e.g. `"S-1-0-0"` to [`sid::Box`]
 pub fn convert_string_sid_to_sid_a(s: impl TryIntoAsCStr) -> Result<SidBox<alloc::LocalAllocFree>, LastError> {
     let s = s.try_into().map_err(|_| LastError(ERROR_INVALID_PARAMETER))?;
     let mut sid = null_mut();
@@ -22,7 +23,8 @@ pub fn convert_string_sid_to_sid_a(s: impl TryIntoAsCStr) -> Result<SidBox<alloc
     else        { sid.ok_or(LastError(ERROR_INVALID_SID)) }
 }
 
-/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertstringsidtosidw)\] ConvertStringSidToSidW
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertstringsidtosidw)\]
+/// ConvertStringSidToSidW - convert e.g. `"S-1-0-0"` to [`sid::Box`]
 pub fn convert_string_sid_to_sid_w(s: impl TryIntoAsCStr<u16>) -> Result<SidBox<alloc::LocalAllocFree>, LastError> {
     let s = s.try_into().map_err(|_| LastError(ERROR_INVALID_PARAMETER))?;
     let mut sid = null_mut();
