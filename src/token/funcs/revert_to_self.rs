354e7665f8c64f8076884159f8e1effbedbaf5d0
MaulingMonkey <git@maulingmonkey.com>
2022-08-08 12:27:56 -0700

    doc: revert_to_self: more examples

--- src/token/funcs/revert_to_self.rs
+++ src/token/funcs/revert_to_self.rs
@@ -3,8 +3,21 @@
 /// ### Example
 /// ```
 /// use win32_security_playground::*;
-/// // TODO: set/reset/verify thread token
-/// revert_to_self().unwrap();
+/// use winapi::shared::winerror::ERROR_BAD_TOKEN_TYPE;
+/// use winapi::um::winnt::{SecurityDelegation, TokenImpersonation};
+/// let token = open_process_token::current_process();
+/// let imp = unsafe { duplicate_token_ex(
+///     &token, token::ALL_ACCESS, None, SecurityDelegation, TokenImpersonation
+/// )};
+///
+/// assert!(open_thread_token::current_thread(false).is_none());
+/// revert_to_self().unwrap(); // wasn't impersonating, should succeed anyways
+/// set_thread_token(None, &imp).unwrap();
+/// assert!(open_thread_token::current_thread(false).is_some());
+/// revert_to_self().unwrap(); // was impersonating
+/// assert!(open_thread_token::current_thread(false).is_none());
+/// revert_to_self().unwrap(); // wasn't impersonating, should succeed anyways
+/// assert!(open_thread_token::current_thread(false).is_none());
 /// ```
 pub fn revert_to_self() -> Result<(), crate::error::LastError> {
     let success = 0 != unsafe { winapi::um::securitybaseapi::RevertToSelf() };
