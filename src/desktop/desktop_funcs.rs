ddb6b6b2bdc04fbadead5a2c68dc30063c9fc820
MaulingMonkey <git@maulingmonkey.com>
2022-08-17 16:01:34 -0700

    {set => with}_thread_desktop

--- src/desktop/desktop_funcs.rs
+++ src/desktop/desktop_funcs.rs
@@ -2,6 +2,7 @@ use crate::*;
 use abistr::*;
 use winapi::ctypes::c_char;
 use winapi::shared::minwindef::{FALSE, LPARAM, BOOL, TRUE};
+use winapi::shared::windef::HDESK;
 use winapi::shared::winerror::*;
 use winapi::um::errhandlingapi::SetLastError;
 use winapi::um::winuser::*;
@@ -17,7 +18,8 @@ use std::ptr::null_mut;
 /// # use win32_security_playground::*;
 /// # use abistr::cstr;
 /// # use winapi::um::winnt::GENERIC_ALL;
-/// let winsta = create_desktop_a(cstr!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// let desktop = create_desktop_a(cstr!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// # let desktop = create_desktop_a(cstr!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
 /// ```
 pub fn create_desktop_a(
     desktop:        impl TryIntoAsCStr,
@@ -43,7 +45,8 @@ pub fn create_desktop_a(
 /// # use win32_security_playground::*;
 /// # use abistr::cstr16;
 /// # use winapi::um::winnt::GENERIC_ALL;
-/// let winsta = create_desktop_w(cstr16!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// let desktop = create_desktop_w(cstr16!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// # let desktop = create_desktop_w(cstr16!("PlaygroundDesktop"), (), None, 0, GENERIC_ALL, None).unwrap();
 /// ```
 pub fn create_desktop_w(
     desktop:        impl TryIntoAsCStr<u16>,
@@ -243,8 +246,59 @@ pub fn open_input_desktop(
 }
 
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setthreaddesktop)\]
-/// SetThreadDesktop
-// TODO: example?
-pub fn set_thread_desktop(desktop: &desktop::OwnedHandle) -> Result<(), LastError> {
-    LastError::get_if(FALSE == unsafe { SetThreadDesktop(desktop.as_handle()) })
+/// SetThreadDesktop x2 + GetThreadDesktop
+///
+/// ### Example
+/// ```
+/// # use win32_security_playground::*;
+/// # use abistr::cstr;
+/// # use winapi::um::winnt::GENERIC_ALL;
+/// # use winapi::um::winuser::*;
+/// let temp1 = create_desktop_a(cstr!("wtd.temp1"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// let temp2 = create_desktop_a(cstr!("wtd.temp2"), (), None, 0, GENERIC_ALL, None).unwrap();
+/// let orig  = get_thread_desktop(get_current_thread_id()).unwrap();
+/// with_thread_desktop(&temp1, || {
+///     with_thread_desktop(&temp2, || {
+///         with_thread_desktop(&temp1, || {
+///             with_thread_desktop(&orig, || {
+///                 // ...
+///             }).unwrap();
+///         }).unwrap();
+///     }).unwrap();
+/// }).unwrap();
+/// ```
+///
+/// ### Errata
+/// Thread ownership of `HDESK`s is a little wonky:
+/// *   `CloseDesktop(desk)` will fail with `GetLastError() == ERROR_BUSY` if any threads are set to use `desk` as their desktops.
+///     This is conceptually similar to the thread having a `std::cell::Ref<'static, Handle>` and panicing with a borrowing error.
+///
+/// *   `SetThreadDesktop(null)` is an error / noop and will not unlock the previously set desktop.
+///
+/// *   `GetThreadDesktop(thread_id)` returns a real handle while noting:
+///     > You do not need to call the `CloseDesktop` function to close the returned handle.
+///
+///     To be clear - this is presumably because whatever code created said desktop is assumed to exclusively own, it and be in charge of closing it if needed.
+///     Your code can just... kinda reborrow it without locking it, restore to it, etc.
+///     You can extend the lifetime via `DuplicateHandle` and then restore the desktop via said duplicate, but then you cannot drop/close said duplicate handle.
+///
+/// *   By strictly enforcing LIFO stacking order / borrowing for the thread's desktops, [`with_thread_desktop`] avoids the
+///     awkward ownership issues of `'static` lifetimes that would be involved with directly exposing SetThreadDesktop.
+pub fn with_thread_desktop<R>(desktop: &desktop::OwnedHandle, f: impl FnOnce()->R) -> Result<R, LastError> {
+    let thread = get_current_thread_id();
+    let original = unsafe { GetThreadDesktop(thread) };
+    let desktop = desktop.as_handle();
+    LastError::get_if(original.is_null())?;
+    LastError::get_if(FALSE == unsafe { SetThreadDesktop(desktop) })?;
+
+    struct RestoreDesktopOnDrop(HDESK);
+    impl Drop for RestoreDesktopOnDrop { fn drop(&mut self) { if !self.0.is_null() { assert_eq!(FALSE, unsafe { SetThreadDesktop(self.0) }) } } }
+    let restore = RestoreDesktopOnDrop(original);
+
+    let r = f();
+
+    debug_assert_eq!(desktop, unsafe { GetThreadDesktop(thread) });
+    std::mem::forget(restore); // manually restore for error codes:
+    LastError::get_if(FALSE == unsafe { SetThreadDesktop(original) })?;
+    Ok(r)
 }
