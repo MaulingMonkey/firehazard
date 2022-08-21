278bc667add62bd6305455dbd8a9e4512611df43
MaulingMonkey <git@maulingmonkey.com>
2022-08-21 06:48:28 -0700

    alloc::ProcessHeapAllocFree

--- src/alloc/_alloc.rs
+++ src/alloc/_alloc.rs
@@ -4,6 +4,11 @@ mod cstring;                    pub use cstring::*;
 
 
 
+/// [`HeapAlloc`](https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapalloc) /
+/// [`HeapFree`](https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapfree) on
+/// [`GetProcessHeap`](https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-getprocessheap)
+pub struct ProcessHeapAllocFree;
+
 /// [`LocalAlloc`](https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-localalloc) /
 /// [`LocalFree`](https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-localfree)
 pub struct LocalAllocFree;
@@ -11,7 +16,8 @@ pub struct LocalAllocFree;
 /// [`FreeSid`](https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-freesid)
 pub struct FreeSid;
 
-/// Deallocates [`SID`](https://docs.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-sid)s
-pub unsafe trait Deallocator                { unsafe fn free<T>(sid: *mut T); }
-unsafe impl Deallocator for LocalAllocFree  { unsafe fn free<T>(sid: *mut T) { assert!(unsafe { winapi::um::winbase::LocalFree(sid.cast()) }.is_null()) } }
-unsafe impl Deallocator for FreeSid         { unsafe fn free<T>(sid: *mut T) { assert!(unsafe { winapi::um::securitybaseapi::FreeSid(sid.cast()) }.is_null()) } }
+/// Deallocates memory
+pub unsafe trait Deallocator                        { unsafe fn free<T>(mem: *mut T); }
+unsafe impl Deallocator for ProcessHeapAllocFree    { unsafe fn free<T>(mem: *mut T) { assert!(unsafe { winapi::um::heapapi::HeapFree(winapi::um::heapapi::GetProcessHeap(), 0, mem.cast()) } != 0) } }
+unsafe impl Deallocator for LocalAllocFree          { unsafe fn free<T>(mem: *mut T) { assert!(unsafe { winapi::um::winbase::LocalFree(mem.cast()) }.is_null()) } }
+unsafe impl Deallocator for FreeSid                 { unsafe fn free<T>(mem: *mut T) { assert!(unsafe { winapi::um::securitybaseapi::FreeSid(mem.cast()) }.is_null()) } }
