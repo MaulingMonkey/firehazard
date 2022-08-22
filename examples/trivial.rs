e906b8fde69e31f68f659be374a12fda4adaaad2
MaulingMonkey <git@maulingmonkey.com>
2022-08-22 12:16:47 -0700

    trivial: revert_to_self, sleep_ms on nightly / nostd

--- examples/trivial.rs
+++ examples/trivial.rs
@@ -2,20 +2,23 @@
 #![cfg_attr(nightly, no_std)]
 #![cfg_attr(nightly, no_main)]
 
+use win32_security_playground::*;
 use abistr::*;
-use win32_security_playground::output_debug_string_a;
 
-#[cfg(not(nightly))] fn main() { sandbox() }
+#[cfg(not(nightly))] fn main() { run() }
 
 #[cfg(nightly)] mod nightly {
-
-    #[no_mangle] pub extern fn mainCRTStartup() -> i32 { crate::sandbox(); 0 }
-    #[no_mangle] pub extern fn main() -> i32 { 0 } // unused?
+    #[no_mangle] pub extern fn mainCRTStartup() -> i32 { crate::run(); 0 }
+    #[no_mangle] pub extern fn main() -> i32 { 0 } // unused but required?
     #[lang = "eh_personality"] extern fn rust_eh_personality() {}
     #[lang = "panic_impl"] extern fn rust_begin_panic(_: &core::panic::PanicInfo) -> ! { core ::intrinsics::abort() }
 }
 
-fn sandbox() {
+fn run() {
+    revert_to_self().unwrap();
     output_debug_string_a(cstr!("sandbox"));
-    #[cfg(all(std, not(nightly)))] std::thread::sleep(std::time::Duration::from_secs(1));
+    sleep_ms(1_000);
 }
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-sleep)\] Sleep
+fn sleep_ms(ms: u32) { unsafe { winapi::um::synchapi::Sleep(ms) } }
