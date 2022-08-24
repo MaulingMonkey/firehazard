7ef5420aab08cd8183b8f5c539d31cb82a16f6e5
MaulingMonkey <git@maulingmonkey.com>
2022-08-24 08:50:12 -0700

    process::CreationFlags cleanup

--- crates/sandbox-windows-ffi/src/process/process_creation_flags.rs
+++ crates/sandbox-windows-ffi/src/process/process_creation_flags.rs
@@ -1,6 +1,4 @@
-use core::convert::Infallible;
 use core::fmt::{self, Debug, Formatter};
-use core::ops::*;
 
 
 
@@ -11,6 +9,8 @@ use core::ops::*;
 /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/procthread/process-creation-flags)\] Process creation flags
 #[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)] pub struct CreationFlags(u32);
+
+flags!(impl .. for CreationFlags(u32) - CreationFlagsMask { });
 
 impl CreationFlagsMask {
     pub fn as_u32(self) -> u32 { self.0 }
@@ -26,57 +26,29 @@ impl CreationFlags {
 
 impl Debug for CreationFlags {
     fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
-        let mut v = self.0;
-        if v == 0 { return write!(fmt, "0") }
-
-        macro_rules! v { ($e:expr) => {{
-            const E : u32 = $e.0;
-            if v & E != 0 {
-                write!(fmt, "{}", stringify!($e))?;
-                v &= !E;
-                if v != 0 { write!(fmt, " | ")?; }
-            }
-        }}}
-
-        v!(DEBUG_PROCESS                    );
-        v!(DEBUG_ONLY_THIS_PROCESS          );
-        v!(CREATE_SUSPENDED                 );
-        v!(DETACHED_PROCESS                 );
-        v!(CREATE_NEW_CONSOLE               );
-        v!(CREATE_NEW_PROCESS_GROUP         );
-        v!(CREATE_UNICODE_ENVIRONMENT       );
-        v!(CREATE_SEPARATE_WOW_VDM          );
-        v!(CREATE_SHARED_WOW_VDM            );
-        v!(INHERIT_PARENT_AFFINITY          );
-        v!(CREATE_PROTECTED_PROCESS         );
-        v!(EXTENDED_STARTUPINFO_PRESENT     );
-        v!(CREATE_SECURE_PROCESS            );
-        v!(CREATE_BREAKAWAY_FROM_JOB        );
-        v!(CREATE_PRESERVE_CODE_AUTHZ_LEVEL );
-        v!(CREATE_DEFAULT_ERROR_MODE        );
-        v!(CREATE_NO_WINDOW                 );
-
-        if v != 0 { write!(fmt, "0x{:X}", v)? }
-
-        Ok(())
+        const CREATE_SECURE_PROCESS : u32 = 0x00400000;
+        use winapi::um::winbase::*;
+        flags!(self.0, fmt, "0x{:X}", [
+            DEBUG_PROCESS,
+            DEBUG_ONLY_THIS_PROCESS,
+            CREATE_SUSPENDED,
+            DETACHED_PROCESS,
+            CREATE_NEW_CONSOLE,
+            CREATE_NEW_PROCESS_GROUP,
+            CREATE_UNICODE_ENVIRONMENT,
+            CREATE_SEPARATE_WOW_VDM,
+            CREATE_SHARED_WOW_VDM,
+            INHERIT_PARENT_AFFINITY,
+            CREATE_PROTECTED_PROCESS,
+            EXTENDED_STARTUPINFO_PRESENT,
+            CREATE_SECURE_PROCESS,
+            CREATE_BREAKAWAY_FROM_JOB,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL,
+            CREATE_DEFAULT_ERROR_MODE,
+            CREATE_NO_WINDOW,
+        ])
     }
 }
-
-impl From<()> for CreationFlags { fn from(_: ()) -> Self { Self(0) } }
-impl From<Option<Infallible>> for CreationFlags { fn from(_: Option<Infallible>) -> Self { Self(0) } }
-impl From<CreationFlags> for u32 { fn from(ar: CreationFlags) -> Self { ar.as_u32() } }
-
-impl BitAnd         for CreationFlags { type Output = Self; fn bitand(self, rhs: Self) -> Self::Output { Self(self.as_u32() & rhs.as_u32()) } }
-impl BitXor         for CreationFlags { type Output = Self; fn bitxor(self, rhs: Self) -> Self::Output { Self(self.as_u32() ^ rhs.as_u32()) } }
-impl BitOr          for CreationFlags { type Output = Self; fn bitor (self, rhs: Self) -> Self::Output { Self(self.as_u32() | rhs.as_u32()) } }
-impl BitAndAssign   for CreationFlags { fn bitand_assign(&mut self, rhs: Self) { self.0 &= rhs.as_u32() } }
-impl BitXorAssign   for CreationFlags { fn bitxor_assign(&mut self, rhs: Self) { self.0 ^= rhs.as_u32() } }
-impl BitOrAssign    for CreationFlags { fn bitor_assign (&mut self, rhs: Self) { self.0 |= rhs.as_u32() } }
-
-impl Not                                for CreationFlags     { type Output = CreationFlagsMask; fn not(self) -> Self::Output { CreationFlagsMask(!self.as_u32()) } }
-impl BitAnd<CreationFlagsMask>          for CreationFlags     { type Output = CreationFlags; fn bitand(self, rhs: CreationFlagsMask) -> CreationFlags { CreationFlags(self.as_u32() & rhs.as_u32()) } }
-impl BitAnd<CreationFlags>              for CreationFlagsMask { type Output = CreationFlags; fn bitand(self, rhs: CreationFlags    ) -> CreationFlags { CreationFlags(self.as_u32() & rhs.as_u32()) } }
-impl BitAndAssign<CreationFlagsMask>    for CreationFlags     { fn bitand_assign(&mut self, rhs: CreationFlagsMask) { self.0 &= rhs.as_u32() } }
 
 /// Make the current process a debugger of the process being launched and it's children.
 pub const DEBUG_PROCESS                     : CreationFlags = CreationFlags(winapi::um::winbase::DEBUG_PROCESS                      ); // 0x00000001
