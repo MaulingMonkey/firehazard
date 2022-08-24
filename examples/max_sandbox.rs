f5307652f474ebce908e72e8cea90b62c64beb0c
MaulingMonkey <git@maulingmonkey.com>
2022-08-24 15:54:39 -0700

    examples/max_sandbox: give children individually downgraded jobs

--- examples/max_sandbox.rs
+++ examples/max_sandbox.rs
@@ -124,56 +124,6 @@ fn main() {
     let context = Context {
         main_desktop:   get_thread_desktop(get_current_thread_id()).unwrap(),
         alt_desktop:    create_desktop_a(cstr!("max_sandbox_desktop"), (), None, 0, access::GENERIC_ALL, None).unwrap(),
-        job: {
-            // TODO: chrome seems to give each process its own job object
-            let mut job = create_job_object_a(None, ()).unwrap();
-            // TODO: consider UserHandleGrantAccess to... do what, exactly?
-            // https://docs.microsoft.com/en-us/windows/win32/api/jobapi2/nf-jobapi2-setinformationjobobject
-            set_information_job_object(&mut job, JOBOBJECT_BASIC_UI_RESTRICTIONS { UIRestrictionsClass: 0
-                | JOB_OBJECT_UILIMIT_DESKTOP            // Prevents processes associated with the job from creating desktops and switching desktops using the CreateDesktop and SwitchDesktop functions.
-                | JOB_OBJECT_UILIMIT_DISPLAYSETTINGS    // Prevents processes associated with the job from calling the ChangeDisplaySettings function.
-                | JOB_OBJECT_UILIMIT_EXITWINDOWS        // Prevents processes associated with the job from calling the ExitWindows or ExitWindowsEx function.
-                | JOB_OBJECT_UILIMIT_GLOBALATOMS        // Prevents processes associated with the job from accessing global atoms. When this flag is used, each job has its own atom table.
-                | JOB_OBJECT_UILIMIT_HANDLES            // Prevents processes associated with the job from using USER handles owned by processes not associated with the same job.
-                | JOB_OBJECT_UILIMIT_READCLIPBOARD      // Prevents processes associated with the job from reading data from the clipboard.
-                | JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS   // Prevents processes associated with the job from changing system parameters by using the SystemParametersInfo function.
-                | JOB_OBJECT_UILIMIT_WRITECLIPBOARD     // Prevents processes associated with the job from writing data to the clipboard.
-            }).unwrap();
-            set_information_job_object(&mut job, JOBOBJECT_EXTENDED_LIMIT_INFORMATION {
-                BasicLimitInformation: JOBOBJECT_BASIC_LIMIT_INFORMATION {
-                    LimitFlags: 0
-                        | JOB_OBJECT_LIMIT_ACTIVE_PROCESS
-                        | JOB_OBJECT_LIMIT_DIE_ON_UNHANDLED_EXCEPTION
-                        | JOB_OBJECT_LIMIT_JOB_MEMORY
-                        | JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
-                        // | JOB_OBJECT_LIMIT_JOB_TIME // ?
-                        ,
-                    ActiveProcessLimit: 1, // TODO: chrome seems to adjust this down to 0 post-launch?
-                    //PerJobUserTimeLimit: ..., // ?
-                    .. unsafe { zeroed() }
-                },
-                JobMemoryLimit: 4 * 1024*1024*1024, // 4 GiB
-                .. unsafe { zeroed() }
-            }).unwrap();
-            #[cfg(nope)] // TODO: the pointers in this type would require set_information_job_object to be `unsafe`, replace with a safer type
-            set_information_job_object(&mut job, &JOBOBJECT_SECURITY_LIMIT_INFORMATION {
-                SecurityLimitFlags: 0
-                    | JOB_OBJECT_SECURITY_NO_ADMIN          // Prevents any process in the job from using a token that specifies the local administrators group.
-                    | JOB_OBJECT_SECURITY_RESTRICTED_TOKEN  // Prevents any process in the job from using a token that was not created with the CreateRestrictedToken function.
-                    // | JOB_OBJECT_SECURITY_FILTER_TOKENS    // Applies a filter to the token when a process impersonates a client. Requires at least one of the following members to be set: SidsToDisable, PrivilegesToDelete, or RestrictedSids.
-                    // | JOB_OBJECT_SECURITY_ONLY_TOKEN       // I don't have JobToken set - and would this prevent SetThreadToken ?
-                    ,
-                .. unsafe { zeroed() }
-            }).unwrap();
-            // TODO: JOBOBJECT_END_OF_JOB_TIME_INFORMATION to hard-terminate the processes of the job?
-            // TODO: JobObjectGroupInformation processor groups?
-            // TODO: JOBOBJECT_LIMIT_VIOLATION_INFORMATION_2 limits?
-            // TODO: JOBOBJECT_NET_RATE_CONTROL_INFORMATION to disable network?
-            // TODO: JOBOBJECT_NOTIFICATION_LIMIT_INFORMATION[_2] ?
-            // TODO: JOBOBJECT_LIMIT_VIOLATION_INFORMATION ?
-            // TODO: SetIoRateControlInformationJobObject ?
-            job
-        },
     };
     for target in Target::list() {
         run(&context, target);
@@ -183,7 +133,6 @@ fn main() {
 struct Context {
     main_desktop:   desktop::OwnedHandle,
     alt_desktop:    desktop::OwnedHandle,
-    job:            job::OwnedHandle,
 }
 
 fn run(context: &Context, target: Target) {
@@ -258,7 +207,8 @@ fn run(context: &Context, target: Target) {
     let mitigation_policy = [policy1, policy2];
     let child_policy = process::creation::child_process::RESTRICTED;
     let dab_policy = process::creation::desktop_app_breakaway::ENABLE_PROCESS_TREE;
-    let job_list = [context.job.clone()];
+    let mut job = create_job();
+    let job_list = [job.clone()];
 
     let attribute_list = process::ThreadAttributeList::try_from(&[
         process::ThreadAttributeRef::mitigation_policy_dword64_2(&mitigation_policy),
@@ -276,15 +226,16 @@ fn run(context: &Context, target: Target) {
     ][..]).unwrap();
 
     let mut si = process::StartupInfoExW::default();
-    si.startup_info.desktop = None; // TODO
     si.startup_info.flags   = STARTF_UNTRUSTEDSOURCE;
     si.attribute_list       = Some(attribute_list);
 
+    // TODO: verify this actually puts the new process in the sandbox desktop
     let pi = with_thread_desktop(desktop, || create_process_as_user_w(
         &restricted, (), Some(unsafe { command_line.buffer_mut() }), None, None, false,
         process::DEBUG_PROCESS | process::CREATE_SEPARATE_WOW_VDM | process::CREATE_SUSPENDED | process::EXTENDED_STARTUPINFO_PRESENT, process::environment::Clear, (), &si
     ).unwrap()).unwrap();
     set_thread_token(&pi.thread, &permissive).unwrap();
+    relimit_job(&mut job, 0);
     resume_thread(&pi.thread).unwrap();
 
     let mut sandboxed = false;
@@ -394,4 +345,75 @@ fn run(context: &Context, target: Target) {
 
     let exit = wait_for_process(pi.process).unwrap();
     assert!(exit == 0, "exit code: 0x{exit:08x} {}", Error::from(exit).friendly());
+}
+
+fn create_job() -> job::OwnedHandle {
+    let mut job = create_job_object_a(None, ()).unwrap();
+    relimit_job(&mut job, 1);
+
+    // TODO: consider UserHandleGrantAccess to... do what, exactly?
+    // https://docs.microsoft.com/en-us/windows/win32/api/jobapi2/nf-jobapi2-setinformationjobobject
+    set_information_job_object(&mut job, JOBOBJECT_BASIC_UI_RESTRICTIONS { UIRestrictionsClass: 0
+        | JOB_OBJECT_UILIMIT_DESKTOP            // Prevents processes associated with the job from creating desktops and switching desktops using the CreateDesktop and SwitchDesktop functions.
+        | JOB_OBJECT_UILIMIT_DISPLAYSETTINGS    // Prevents processes associated with the job from calling the ChangeDisplaySettings function.
+        | JOB_OBJECT_UILIMIT_EXITWINDOWS        // Prevents processes associated with the job from calling the ExitWindows or ExitWindowsEx function.
+        | JOB_OBJECT_UILIMIT_GLOBALATOMS        // Prevents processes associated with the job from accessing global atoms. When this flag is used, each job has its own atom table.
+        | JOB_OBJECT_UILIMIT_HANDLES            // Prevents processes associated with the job from using USER handles owned by processes not associated with the same job.
+        | JOB_OBJECT_UILIMIT_READCLIPBOARD      // Prevents processes associated with the job from reading data from the clipboard.
+        | JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS   // Prevents processes associated with the job from changing system parameters by using the SystemParametersInfo function.
+        | JOB_OBJECT_UILIMIT_WRITECLIPBOARD     // Prevents processes associated with the job from writing data to the clipboard.
+    }).unwrap();
+    set_information_job_object(&mut job, JOBOBJECT_EXTENDED_LIMIT_INFORMATION {
+        BasicLimitInformation: JOBOBJECT_BASIC_LIMIT_INFORMATION {
+            LimitFlags: 0
+                | JOB_OBJECT_LIMIT_ACTIVE_PROCESS
+                | JOB_OBJECT_LIMIT_DIE_ON_UNHANDLED_EXCEPTION
+                | JOB_OBJECT_LIMIT_JOB_MEMORY
+                | JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+                // | JOB_OBJECT_LIMIT_JOB_TIME // ?
+                ,
+            ActiveProcessLimit: 1, // TODO: chrome seems to adjust this down to 0 post-launch?
+            //PerJobUserTimeLimit: ..., // ?
+            .. unsafe { zeroed() }
+        },
+        JobMemoryLimit: 4 * 1024*1024*1024, // 4 GiB
+        .. unsafe { zeroed() }
+    }).unwrap();
+    #[cfg(nope)] // TODO: the pointers in this type would require set_information_job_object to be `unsafe`, replace with a safer type
+    set_information_job_object(&mut job, JOBOBJECT_SECURITY_LIMIT_INFORMATION {
+        SecurityLimitFlags: 0
+            | JOB_OBJECT_SECURITY_NO_ADMIN          // Prevents any process in the job from using a token that specifies the local administrators group.
+            | JOB_OBJECT_SECURITY_RESTRICTED_TOKEN  // Prevents any process in the job from using a token that was not created with the CreateRestrictedToken function.
+            // | JOB_OBJECT_SECURITY_FILTER_TOKENS    // Applies a filter to the token when a process impersonates a client. Requires at least one of the following members to be set: SidsToDisable, PrivilegesToDelete, or RestrictedSids.
+            // | JOB_OBJECT_SECURITY_ONLY_TOKEN       // I don't have JobToken set - and would this prevent SetThreadToken ?
+            ,
+        .. unsafe { zeroed() }
+    }).unwrap();
+    // TODO: JOBOBJECT_END_OF_JOB_TIME_INFORMATION to hard-terminate the processes of the job?
+    // TODO: JobObjectGroupInformation processor groups?
+    // TODO: JOBOBJECT_LIMIT_VIOLATION_INFORMATION_2 limits?
+    // TODO: JOBOBJECT_NET_RATE_CONTROL_INFORMATION to disable network?
+    // TODO: JOBOBJECT_NOTIFICATION_LIMIT_INFORMATION[_2] ?
+    // TODO: JOBOBJECT_LIMIT_VIOLATION_INFORMATION ?
+    // TODO: SetIoRateControlInformationJobObject ?
+    job
+}
+
+fn relimit_job(job: &mut job::OwnedHandle, processes: u32) {
+    set_information_job_object(job, JOBOBJECT_EXTENDED_LIMIT_INFORMATION {
+        BasicLimitInformation: JOBOBJECT_BASIC_LIMIT_INFORMATION {
+            LimitFlags: 0
+                | JOB_OBJECT_LIMIT_ACTIVE_PROCESS
+                | JOB_OBJECT_LIMIT_DIE_ON_UNHANDLED_EXCEPTION
+                | JOB_OBJECT_LIMIT_JOB_MEMORY
+                | JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+                // | JOB_OBJECT_LIMIT_JOB_TIME // ?
+                ,
+            ActiveProcessLimit: processes,
+            //PerJobUserTimeLimit: ..., // ?
+            .. unsafe { zeroed() }
+        },
+        JobMemoryLimit: 4 * 1024*1024*1024, // 4 GiB
+        .. unsafe { zeroed() }
+    }).unwrap();
 }
