091a17ea96c1bda40a5b1d0ca31ec6f23f10b4b0
MaulingMonkey <git@maulingmonkey.com>
2022-08-28 16:43:12 -0700

    cull crates/{sandbox,sandbox-windows}

--- crates/sandbox-windows/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows) specific sandboxing APIs
-
-#![cfg_attr(not(std), no_std)]
-#![forbid(unsafe_op_in_unsafe_fn)]
-#![deny(unreachable_patterns)]
-
-#[doc(inline)] pub extern crate sandbox_windows_ffi as ffi;
