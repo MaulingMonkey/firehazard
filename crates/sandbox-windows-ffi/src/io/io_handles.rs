6742ae35fb47c3ef9f803feee3a3d65ebf4cc4b2
MaulingMonkey <git@maulingmonkey.com>
2022-08-28 10:41:02 -0700

    io::File, #[repr(transparent)] pipes

--- crates/sandbox-windows-ffi/src/io/io_handles.rs
+++ crates/sandbox-windows-ffi/src/io/io_handles.rs
@@ -1,16 +1,31 @@
 use crate::*;
+
 use winapi::ctypes::c_void;
 use winapi::shared::minwindef::FALSE;
 use winapi::um::fileapi::{ReadFile, WriteFile};
+
+#[cfg(std)] use std::os::windows::prelude::*;
+
 use core::ptr::null_mut;
 
+// TODO: consider implementing std::os::windows::io::* interop traits
+// https://doc.rust-lang.org/std/os/windows/io/index.html
 
 
-/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-createpipe)\] Owned anonymous pipe `HANDLE` (not repr transparent! [Read]able end)
-pub struct ReadPipe (pub(super) HANDLENN);
 
-/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-createpipe)\] Owned anonymous pipe `HANDLE` (not repr transparent! [Write]able end)
-pub struct WritePipe(pub(super) HANDLENN);
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-createpipe)\] Owned non-null file `HANDLE`
+#[repr(transparent)] pub struct File (pub(super) HANDLENN);
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-createpipe)\] Owned anonymous non-null pipe `HANDLE` ([Read]able end)
+#[repr(transparent)] pub struct ReadPipe (pub(super) HANDLENN);
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-createpipe)\] Owned anonymous non-null pipe `HANDLE` ([Write]able end)
+#[repr(transparent)] pub struct WritePipe(pub(super) HANDLENN);
+
+handles!(impl *LocalHandleNN<c_void>    for io::{File});
+handles!(impl Debug                     for io::{File});
+handles!(impl {AsRef, From}             for io::{File});
+handles!(impl {AsRef<@base>, From}      for io::{File});
 
 handles!(impl *LocalHandleNN<c_void>    for io::{ReadPipe});
 handles!(impl Debug                     for io::{ReadPipe});
@@ -22,25 +37,32 @@ handles!(impl Debug                     for io::{WritePipe});
 handles!(impl {AsRef, From}             for io::{WritePipe});
 handles!(impl {AsRef<@base>, From}      for io::{WritePipe});
 
-impl Drop for ReadPipe  { fn drop(&mut self) { unsafe { drop_close_handle_nn(self) } } }
-impl Drop for WritePipe { fn drop(&mut self) { unsafe { drop_close_handle_nn(self) } } }
+impl Drop       for File        { fn drop(&mut self) { unsafe { drop_close_handle_nn(self) } } }
+impl Drop       for ReadPipe    { fn drop(&mut self) { unsafe { drop_close_handle_nn(self) } } }
+impl Drop       for WritePipe   { fn drop(&mut self) { unsafe { drop_close_handle_nn(self) } } }
 
-impl io::Read for ReadPipe {
-    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-readfile)\]
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let mut read = 0;
-        Error::get_last_if(FALSE == unsafe { ReadFile(self.0.as_ptr(), buf.as_mut_ptr().cast(), buf.len().try_into().unwrap_or(!0u32), &mut read, null_mut()) })?;
-        Ok(usize::from32(read))
-    }
+impl io::Read   for File        { fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> { unsafe { read_file(self.0, buf) } } }
+impl io::Read   for ReadPipe    { fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> { unsafe { read_file(self.0, buf) } } }
+
+impl io::Write  for File        { fn write(&mut self, buf: &[u8]) -> io::Result<usize> { unsafe { write_file(self.0, buf) } } fn flush(&mut self) -> io::Result<()> { Ok(()) } }
+impl io::Write  for WritePipe   { fn write(&mut self, buf: &[u8]) -> io::Result<usize> { unsafe { write_file(self.0, buf) } } fn flush(&mut self) -> io::Result<()> { Ok(()) } }
+
+#[cfg(std)] impl From<std::fs::File> for File { fn from(file: std::fs::File ) -> Self { Self(HANDLENN::new(file.into_raw_handle().cast()).unwrap()) } }
+#[cfg(std)] impl From<File> for std::fs::File { fn from(file: File          ) -> Self { unsafe { std::fs::File::from_raw_handle(file.as_handle().cast()) } } }
+
+
+
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-readfile)\] ReadFile
+unsafe fn read_file(h: HANDLENN, buf: &mut [u8]) -> io::Result<usize> {
+    let mut read = 0;
+    Error::get_last_if(FALSE == unsafe { ReadFile(h.as_ptr(), buf.as_mut_ptr().cast(), buf.len().try_into().unwrap_or(!0u32), &mut read, null_mut()) })?;
+    Ok(usize::from32(read))
 }
 
-impl io::Write for WritePipe {
-    fn flush(&mut self) -> io::Result<()> { Ok(()) }
-
-    /// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-writefile)\]
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut written = 0;
-        Error::get_last_if(FALSE == unsafe { WriteFile(self.0.as_ptr(), buf.as_ptr().cast(), buf.len().try_into().unwrap_or(!0u32), &mut written, null_mut()) })?;
-        Ok(usize::from32(written))
-    }
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-writefile)\] WriteFile
+unsafe fn write_file(h: HANDLENN, buf: &[u8]) -> io::Result<usize> {
+    let mut written = 0;
+    Error::get_last_if(FALSE == unsafe { WriteFile(h.as_ptr(), buf.as_ptr().cast(), buf.len().try_into().unwrap_or(!0u32), &mut written, null_mut()) })?;
+    Ok(usize::from32(written))
 }
