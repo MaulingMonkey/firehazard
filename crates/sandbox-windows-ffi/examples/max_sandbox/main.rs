0b569aa512a46d7aed3cffb958640b811eb06d4c
MaulingMonkey <git@maulingmonkey.com>
2022-08-28 21:23:23 -0700

    max_sandbox::tokens

--- crates/sandbox-windows-ffi/examples/max_sandbox/main.rs
+++ crates/sandbox-windows-ffi/examples/max_sandbox/main.rs
@@ -1,6 +1,7 @@
 mod attribute;
 mod job;
 mod settings;
+mod tokens;
 
 use sandbox_windows_ffi::*;
 
@@ -36,48 +37,11 @@ struct Context {
 fn run(_context: &Context, target: settings::Target) {
     assert!(target.spawn.integrity >= target.lockdown.integrity, "target.lockdown.integrity cannot be more permissive than spawn integrity");
 
-    let sandbox_process_token = open_process_token(get_current_process(), token::ALL_ACCESS).unwrap();
-    let gap = sandbox_process_token.groups_and_privileges().unwrap();
-    let all_group_sids = gap.sids().iter().map(|g| g.sid).collect::<Vec<_>>();
-    let logon_session_sid = sandbox_process_token.logon_sid().unwrap();
-    let logon_session_sid = logon_session_sid.groups()[0].sid;
-
-    let permissive = create_restricted_token_filter(
-        &sandbox_process_token,
-        None,
-        |saa| !target.spawn.enabled.iter().any(|e| *saa.sid == **e),
-        |p| !target.spawn.privileges.contains(&p.luid),
-        Some(&target.spawn.restricted.as_ref().unwrap_or(&all_group_sids).iter().copied().map(|r| sid::AndAttributes::new(r, 0)).collect::<Vec<_>>()[..]),
-    ).unwrap();
-
-    let restricted = create_restricted_token_filter(
-        &sandbox_process_token,
-        None,
-        |saa| !target.lockdown.enabled.iter().any(|e| *saa.sid == **e),
-        |p| !target.lockdown.privileges.contains(&p.luid),
-        Some(&target.lockdown.restricted.as_ref().unwrap_or(&all_group_sids).iter().copied().map(|r| sid::AndAttributes::new(r, 0)).collect::<Vec<_>>()[..]),
-    ).unwrap();
-
-    permissive.set_integrity_level(sid::AndAttributes::new(target.spawn.integrity.sid(), 0)).unwrap();
-    restricted.set_integrity_level(sid::AndAttributes::new(target.spawn.integrity.sid(), 0)).unwrap(); // lower child token to target.lockdown.integrity post-spawn
-    let permissive = duplicate_token_ex(&permissive, token::ALL_ACCESS, None, security::Impersonation, token::Impersonation).unwrap(); // primary -> impersonation token
-
-    if false { // the need for this is currently being eliminated via abuse of the debugger APIs
-        // For the child process to lower itself to untrusted integrity, in needs `token::ADJUST_DEFAULT` access
-        // under the thread's current access token (currently done before `revert_to_self()`, so `permissive`).
-        // `permissive` is currently restricted to only "Everyone" and "LogonSession_x_yyyyyyy" - the latter seems narrower, so we grant access to it.
-        let mut acl = acl::Builder::new(acl::REVISION);
-        acl.add_acl(acl::REVISION, 0, restricted.default_dacl().unwrap().default_dacl()).unwrap(); // allow debuggers to attach, task managers to kill, etc.
-        acl.add_access_allowed_ace(acl::REVISION, token::ADJUST_DEFAULT | token::QUERY, logon_session_sid).unwrap();
-        acl.finish().unwrap();
-        restricted.set_default_dacl(&mut acl).unwrap();
-    }
-
+    let tokens = tokens::create(&target);
     let mut command_line = abistr::CStrBuf::<u16, 32768>::from_truncate(&target.exe.as_os_str().encode_wide().collect::<Vec<_>>());
 
     let job = job::create();
     let attribute_list = attribute::List::new(&target, &job);
-
     let mut si = process::StartupInfoExW::default();
     // N.B.: this will cause user32.dll(?) to STATUS_DLL_INIT_FAILED unless the child process can access the named desktop
     // specifying a nonexistant desktop is also an option
@@ -86,10 +50,10 @@ fn run(_context: &Context, target: settings::Target) {
     si.attribute_list       = Some(attribute_list.to_list());
 
     let pi = create_process_as_user_w(
-        &restricted, (), Some(unsafe { command_line.buffer_mut() }), None, None, false,
+        &tokens.restricted, (), Some(unsafe { command_line.buffer_mut() }), None, None, false,
         process::DEBUG_PROCESS | process::CREATE_SEPARATE_WOW_VDM | process::CREATE_SUSPENDED | process::EXTENDED_STARTUPINFO_PRESENT, process::environment::Clear, (), &si
     ).unwrap();
-    set_thread_token(&pi.thread, &permissive).unwrap();
+    set_thread_token(&pi.thread, &tokens.permissive).unwrap();
     job::relimit(&job, 0);
     resume_thread(&pi.thread).unwrap();
 
@@ -126,7 +90,7 @@ fn run(_context: &Context, target: settings::Target) {
                 assert!(FALSE != unsafe { DuplicateHandle(process, thread, process, &mut thread, access::GENERIC_ALL.into(), false as _, 0) });
                 let thread = unsafe { thread::OwnedHandle::from_raw(thread) }.unwrap();
 
-                set_thread_token(&thread, &permissive).unwrap();
+                set_thread_token(&thread, &tokens.permissive).unwrap();
                 let _prev_thread = threads.insert(dwThreadId, thread);
                 debug_assert!(_prev_thread.is_none());
                 dbg_continue();
@@ -139,7 +103,7 @@ fn run(_context: &Context, target: settings::Target) {
                 assert!(FALSE != unsafe { DuplicateHandle(process, thread, process, &mut thread, access::GENERIC_ALL.into(), false as _, 0) });
                 let thread = unsafe { thread::OwnedHandle::from_raw(thread) }.unwrap();
 
-                set_thread_token(&thread, &permissive).unwrap(); // already set?
+                set_thread_token(&thread, &tokens.permissive).unwrap(); // already set?
                 let _prev_thread = threads.insert(dwThreadId, thread);
                 debug_assert!(_prev_thread.is_none());
                 dbg_continue();
