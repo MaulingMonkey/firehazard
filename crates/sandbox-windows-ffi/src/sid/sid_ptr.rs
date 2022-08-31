9978480ac2e616a21dcd8c069bba03c235fb8909
MaulingMonkey <git@maulingmonkey.com>
2022-08-31 13:19:55 -0700

    BoxTokenUser::from_raw: make safe

--- crates/sandbox-windows-ffi/src/sid/sid_ptr.rs
+++ crates/sandbox-windows-ffi/src/sid/sid_ptr.rs
@@ -1,9 +1,12 @@
 use crate::*;
 
-use winapi::um::winnt::SID;
+use winapi::shared::minwindef::{BYTE, DWORD};
+use winapi::shared::winerror::*;
+use winapi::um::winnt::{SID, SID_IDENTIFIER_AUTHORITY};
 
 use core::fmt::{self, Debug, Formatter};
 use core::marker::PhantomData;
+use core::mem::size_of;
 use core::ops::Deref;
 
 
@@ -20,4 +23,27 @@ impl Ptr<'_> {
     /// `sid` should be null, or point to a valid [`SID`](https://docs.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-sid)
     /// for the lifetime of the [`sid::Ptr`].
     pub const unsafe fn from_raw_unchecked(sid: *mut SID) -> Self { Self(sid, PhantomData) }
+
+    /// ### Safety
+    /// `sid` should be null, or point to a valid [`SID`](https://docs.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-sid)
+    /// for the lifetime of the [`sid::Ptr`].
+    pub unsafe fn from_raw(sid: *mut SID, bytes: usize) -> Result<Self, Error> {
+        if sid.is_null() { return Ok(Self(sid, PhantomData)) }
+        if bytes < size_of::<Header>() { return Err(Error(ERROR_INVALID_SID)) }
+        let header : Header = unsafe { core::ptr::read(sid.cast()) };
+        if header.SubAuthorityCount > 15 { return Err(Error(ERROR_INVALID_SUB_AUTHORITY)) }
+        let expected_size = size_of::<Header>() + size_of::<DWORD>() * usize::from(header.SubAuthorityCount);
+        if bytes < expected_size { return Err(Error(ERROR_INVALID_SID)) }
+        Ok(Self(sid, PhantomData))
+    }
+}
+
+/// SID, but without the trailing variable length SubAuthority field
+#[allow(non_snake_case)]
+#[derive(Clone, Copy)]
+#[repr(C)] struct Header {
+    Revision:               BYTE,
+    SubAuthorityCount:      BYTE,
+    IdentifierAuthority:    SID_IDENTIFIER_AUTHORITY,
+    // SubAuthority: [DWORD; 1 .. 15], // varlen based on SubAuthorityCount
 }
