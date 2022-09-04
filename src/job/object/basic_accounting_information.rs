d55cc8ec65e6e85f0b407c70d6d43d03e14ee830
MaulingMonkey <git@maulingmonkey.com>
2022-09-04 00:45:46 -0700

    job::object::BasicAndIoAccountingInformation

--- src/job/object/basic_accounting_information.rs
+++ src/job/object/basic_accounting_information.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use winapi::um::winnt::{JOBOBJECT_BASIC_ACCOUNTING_INFORMATION, JobObjectBasicAccountingInformation};
+use winapi::um::winnt::*;
 use core::mem::{align_of, size_of};
 
 
@@ -21,7 +21,23 @@ const _SIZE_BASIC : () = assert!(size_of ::<BasicAccountingInformation>() == siz
     pub total_terminated_processes:     u32,
 }
 
-impl job::QueryInformation for JOBOBJECT_BASIC_ACCOUNTING_INFORMATION   { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAccountingInformation) } } }
-impl job::QueryInformation for job::object::BasicAccountingInformation  { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAccountingInformation) } } }
+
+
+const _ALIGN_BASIC_AND_IO : () = assert!(align_of::<BasicAndIoAccountingInformation>() == align_of::<JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION>());
+const _SIZE_BASIC_AND_IO  : () = assert!(size_of ::<BasicAndIoAccountingInformation>() == size_of ::<JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION>());
+
+/// \[[docs.microsoft.com](https://docs.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-jobobject_basic_and_io_accounting_information)\]
+/// JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION
+#[repr(C)] pub struct BasicAndIoAccountingInformation {
+    pub basic_info: BasicAccountingInformation,
+    pub io_info:    io::Counters,
+}
+
+
+
+impl job::QueryInformation for JOBOBJECT_BASIC_ACCOUNTING_INFORMATION           { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAccountingInformation) } } }
+impl job::QueryInformation for job::object::BasicAccountingInformation          { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAccountingInformation) } } }
+impl job::QueryInformation for JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION    { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAndIoAccountingInformation) } } }
+impl job::QueryInformation for job::object::BasicAndIoAccountingInformation     { fn query_from(job: &job::OwnedHandle) -> Result<Self, Error> { unsafe { job::query_fixed(job, JobObjectBasicAndIoAccountingInformation) } } }
 
 // not implemented: job::SetInformation
