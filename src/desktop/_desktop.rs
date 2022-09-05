7981b2860aba0e749a0def1e04d68a6816d7d404
MaulingMonkey <git@maulingmonkey.com>
2022-09-04 22:13:50 -0700

    doc: desktop: comment on visible desktop behavior

--- src/desktop/_desktop.rs
+++ src/desktop/_desktop.rs
@@ -15,7 +15,7 @@
 //! *   [Chrome](https://github.com/chromium/chromium/blob/main/docs/design/sandbox.md#the-alternate-desktop) uses a non-visible desktop for sandboxing code that uses `user32.dll`
 //! *   [KeePass Password Safe's Secure Desktop Option](https://keepass.info/help/kb/sec_desk.html) (opt-in for secure password entry)
 //!
-//! Desktop objects are *not* used by Windows 10+ "virtual" desktops (...I think):
+//! Desktop objects are *not* used by Windows 10+ "virtual" desktops:
 //! *   `Ctrl`+`Win`+`D`:   Create
 //! *   `Ctrl`+`Win`+`F4`:  Close
 //! *   `Ctrl`+`Win`+`🠞`:   Switch Right
@@ -25,14 +25,34 @@
 //! The default permissions of [`create_desktop_a`] may require [`sid::integrity::Medium`] to access, whereas the built-in desktop might only require [`sid::integrity::Low`].
 //! Using `set_thread_desktop` / [`with_thread_desktop`] will *not* set the desktop of processes spawned by said thread.
 //!
-//! On my system, by default, the following desktops exist naturally:
-//! *   `WinSta0\Default`
-//! *   `Service-0x0-21cd8$\Default` (or similar)
+//! On my system, by default, the following desktops exist for the logged in user:
+//! *   `WinSta0\Default` (the current/default/real desktop of said window station)
+//! *   `Service-0x0-21cd8$\Default` (interactive services desktop)
 //!
-//! Since custom desktop objects lack an associated `explorer.exe` process, most [General Windows Shortcuts](https://ss64.com/nt/syntax-keyboard.html) aren't in use.
-//! The following still work:
-//! *   `Ctrl`+`Shift`+`Esc`:   Task Manager
-//! *   `Ctrl`+`Alt`+`Del`:     Windows Security Screen
+//! Other desktops owned by the system / WinLogin may not be visible to enumeration.
+//!
+//!
+//!
+//! ## Visible Desktops
+//!
+//! You can trivially [`create_desktop_a`] and [`switch_desktop`] to a new desktop.
+//! Such new desktops lack an associated `explorer.exe` process / shell by default, meaning:
+//! *   No desktop background
+//! *   No start menu / taskbar
+//! *   Most [General Windows Shortcuts](https://ss64.com/nt/syntax-keyboard.html) aren't in use or available to conflict with your own shortcuts.
+//!     The following still work however:
+//!     *   `Ctrl`+`Shift`+`Esc`:   Task Manager (lets you run commands in a pinch via the File menu)
+//!     *   `Ctrl`+`Alt`+`Del`:     Windows Security Screen
+//!
+//! Windows 10 is pretty good about not letting you get stuck on the desktop.
+//! It appears that if there is no process running on the desktop, and no process has open handles to said desktop,
+//! you'll be booted back to the `Default` desktop and said desktop will be destroyed,
+//! without the need for any watchdog processes or other cleanup.
+//!
+//! Of course, if you leak or hold onto a desktop handle from a long running process without any visible UI on said desktop,
+//! the user might need to hunt down the offending process to kill it.
+//!
+//!
 //!
 //! ### See Also
 //! *   [mod `winsta`](crate::winsta)   for window station APIs and references
