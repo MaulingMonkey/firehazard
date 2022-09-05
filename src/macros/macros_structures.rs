3df41bd27552a7c87dc9e99487b733179d243e00
MaulingMonkey <git@maulingmonkey.com>
2022-09-05 16:09:13 -0700

    doc: structure!

--- src/macros/macros_structures.rs
+++ src/macros/macros_structures.rs
@@ -1,3 +1,19 @@
+/// ### Example
+///
+/// ```
+/// # #[cfg(nope)] { // macro not exported from library
+/// # use firehazard::*;
+/// # use winapi::um::winnt::*;
+/// structure!(@assert layout io::Counters => IO_COUNTERS {
+///     read_operation_count    == ReadOperationCount,
+///     write_operation_count   == WriteOperationCount,
+///     other_operation_count   == OtherOperationCount,
+///     read_transfer_count     == ReadTransferCount,
+///     write_transfer_count    == WriteTransferCount,
+///     other_transfer_count    == OtherTransferCount,
+/// });
+/// # }
+/// ```
 macro_rules! structure {
     ( @assert layout $a:ty => $b:ty { $( $af:ident == $bf:ident ),* $(,)? } ) => {
         const _ : () = assert!(core::mem::align_of::<$a>() == core::mem::align_of::<$b>(), "struct alignment mismatch");
