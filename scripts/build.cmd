c24fd21e2177f4341ac3995bd322f0b4348a4498
MaulingMonkey <git@maulingmonkey.com>
2025-04-01 03:45:58 -0700

    scripts\build.cmd: simplify style

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,13 +1,10 @@
-@cargo +nightly --version >NUL 2>NUL && call "%~dp0.\build-nightly.cmd"
-@if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-@call "%~dp0.\build-no-features.cmd"
-@if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-@call "%~dp0.\build-basic.cmd"
-@if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-@call "%~dp0.\build-i686.cmd"
-@if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+@cargo +nightly --version >NUL 2>NUL || goto :skip-nightly
+@   call "%~dp0.\build-nightly.cmd"                                     || goto :err
+:skip-nightly
+@call "%~dp0.\build-no-features.cmd"                                    || goto :err
+@call "%~dp0.\build-basic.cmd"                                          || goto :err
+@call "%~dp0.\build-i686.cmd"                                           || goto :err
 
 @echo BUILD SUCCESS BUILD SUCCESS BUILD SUCCESS BUILD SUCCESS BUILD SUCCESS
+:err
+@exit /b %ERRORLEVEL%
