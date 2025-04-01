43a41815342f82e5416d476293cd98424cc731c6
MaulingMonkey <git@maulingmonkey.com>
2025-04-01 03:25:20 -0700

    crates\no-std\examples\trivial.rs: explicit `extern` ABI

--- crates/no-std/examples/trivial.rs
+++ crates/no-std/examples/trivial.rs
@@ -4,9 +4,9 @@
 use firehazard::*;
 use abistr::*;
 
-#[no_mangle] extern fn mainCRTStartup() -> i32 { run(); 0 }
-#[no_mangle] extern fn main() -> i32 { 0 } // unused but required?
-#[no_mangle] extern fn __CxxFrameHandler3() -> ! { exit_process(0xC44) } // workaround precompiled libcore using panic = "unwind" per https://github.com/rust-lang/rust/issues/45492#issuecomment-470577653
+#[no_mangle] extern "C" fn mainCRTStartup() -> i32 { run(); 0 }
+#[no_mangle] extern "C" fn main() -> i32 { 0 } // unused but required?
+#[no_mangle] extern "C" fn __CxxFrameHandler3() -> ! { exit_process(0xC44) } // workaround precompiled libcore using panic = "unwind" per https://github.com/rust-lang/rust/issues/45492#issuecomment-470577653
 #[panic_handler] fn panic(_: &core::panic::PanicInfo) -> ! { exit_process(0xFA71C) }
 
 fn run() {
