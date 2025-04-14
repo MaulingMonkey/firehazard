b50fe66a48a557211ad8b2cc78f340dcba23d48e
MaulingMonkey <git@maulingmonkey.com>
2025-04-13 22:17:44 -0700

    soundness fix for convert_sid_to_string_sid_w
    closes https://github.com/MaulingMonkey/firehazard/issues/53

--- src/alloc/cstring.rs
+++ src/alloc/cstring.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-use abistr::CStrPtr;
+use abistr::{CStrPtr, Unit};
 
 use ialloc::meta::Stateless;
 use ialloc::thin::Free;
@@ -11,14 +11,21 @@ use core::marker::PhantomData;
 
 
 #[repr(transparent)] pub struct CString<C, A: Free + Stateless>(*mut C, PhantomData<A>);
+
 impl<C, A: Free + Stateless> CString<C, A> {
     pub const unsafe fn from_raw(raw: *mut C) -> Self { Self(raw, PhantomData) }
 }
+
 impl<A: Free + Stateless> CString<u8, A> {
     #[cfg(    std )] pub fn to_string_lossy<'s>(&'s self) -> std::borrow::Cow<'s, str> { self.as_cstr_ptr().to_string_lossy() }
     #[cfg(not(std))]     fn to_string_lossy<'s>(&'s self) -> &'s str                   { self.as_cstr_ptr().to_str().unwrap() }
-    fn as_cstr_ptr<'s>(&'s self) -> CStrPtr<'s, u8> { unsafe { CStrPtr::from_ptr_unbounded(self.0.cast()) } }
 }
+
+impl<C: Unit, A: Free + Stateless> CString<C, A> {
+    const fn as_cstr_ptr<'s>(&'s self) -> CStrPtr<'s, C> { unsafe { CStrPtr::from_ptr_unbounded(self.0.cast()) } }
+}
+
 impl<C, A: Free + Stateless> Drop    for CString<C,  A> { fn drop(&mut self) { unsafe { A::default().free_nullable(self.0.cast()) } } }
-impl<   A: Free + Stateless> Debug   for CString<u8, A> { fn fmt(&self, fmt: &mut Formatter) -> fmt::Result { write!(fmt, "{:?}", &*self.to_string_lossy()) } }
+impl<   A: Free + Stateless> Debug   for CString<u8, A> { fn fmt(&self, fmt: &mut Formatter) -> fmt::Result { Debug::fmt(&self.as_cstr_ptr(), fmt) } }
+impl<   A: Free + Stateless> Debug   for CString<u16,A> { fn fmt(&self, fmt: &mut Formatter) -> fmt::Result { Debug::fmt(&self.as_cstr_ptr(), fmt) } }
 impl<   A: Free + Stateless> Display for CString<u8, A> { fn fmt(&self, fmt: &mut Formatter) -> fmt::Result { write!(fmt, "{}", self.to_string_lossy()) } }
--- src/sid/funcs/convert_sid_to_string_sid.rs
+++ src/sid/funcs/convert_sid_to_string_sid.rs
@@ -2,6 +2,12 @@
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertsidtostringsida)\]
 /// ConvertSidToStringSidA
 ///
+/// ### Example
+/// ```
+/// # use firehazard::*;
+/// assert_eq!("S-1-2-3", convert_sid_to_string_sid_a(&*sid!(S-1-2-3)).unwrap().to_string());
+/// ```
+///
 pub fn convert_sid_to_string_sid_a(sid: &crate::sid::Value) -> Option<crate::alloc::CString<u8, crate::alloc::LocalAllocFree>> {
     if sid.as_psid().is_null() { return None }
     let mut local_string = core::ptr::null_mut();
@@ -18,10 +24,17 @@ pub fn convert_sid_to_string_sid_a(sid: &crate::sid::Value) -> Option<crate::all
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/sddl/nf-sddl-convertsidtostringsidw)\]
 /// ConvertSidToStringSidW
 ///
+/// ### Example
+/// ```
+/// # use abistr::*;
+/// # use firehazard::*;
+/// assert!("\"S-1-2-3\"" == format!("{:?}", convert_sid_to_string_sid_w(&*sid!(S-1-2-3)).unwrap()));
+/// ```
+///
 pub fn convert_sid_to_string_sid_w(sid: &crate::sid::Value) -> Option<crate::alloc::CString<u16, crate::alloc::LocalAllocFree>> {
     if sid.as_psid().is_null() { return None }
     let mut local_string = core::ptr::null_mut();
-    let succeeded = 0 != unsafe { winapi::shared::sddl::ConvertSidToStringSidA(sid.as_psid(), &mut local_string) };
+    let succeeded = 0 != unsafe { winapi::shared::sddl::ConvertSidToStringSidW(sid.as_psid(), &mut local_string) };
     let local_string = unsafe { crate::alloc::CString::from_raw(local_string.cast()) };
     assert!(succeeded, "ConvertSidToStringSidW");
     Some(local_string)
