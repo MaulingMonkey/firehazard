651ea8048599a99528bbfa37b195ebea9c2e5d5f
MaulingMonkey <git@maulingmonkey.com>
2025-04-16 21:59:10 -0700

    exit_thread: assume it won't return

--- src/thread/funcs/exit_thread.rs
+++ src/thread/funcs/exit_thread.rs
@@ -2,4 +2,11 @@
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-exitthread)\]
 /// ExitThread
 ///
-pub fn exit_thread(exit_code: u32) { unsafe { winapi::um::processthreadsapi::ExitThread(exit_code) } }
+pub fn exit_thread(exit_code: u32) -> ! {
+    unsafe { winapi::um::processthreadsapi::ExitThread(exit_code) };
+    if cfg!(debug_assertions) {
+        panic!("undefined behavior: ExitThread returned, but the caller's thread should've exited!");
+    } else {
+        unsafe { core::hint::unreachable_unchecked() }
+    }
+}
