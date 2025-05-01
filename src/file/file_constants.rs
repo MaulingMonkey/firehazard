3d117ee07363e9a8dcab860a1c0f9949c72e9151
MaulingMonkey <git@maulingmonkey.com>
2025-04-30 20:03:45 -0700

    file_constants.rs: hex literals reads better in generated docs

--- src/file/file_constants.rs
+++ src/file/file_constants.rs
@@ -58,16 +58,19 @@ pub const TYPE_REMOTE  : u32 = 0x8000;
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea)\]
 /// FILE_FLAG_WRITE_THROUGH
 ///
-pub const FLAG_WRITE_THROUGH : u32 = winapi::um::winbase::FILE_FLAG_WRITE_THROUGH;
+pub const FLAG_WRITE_THROUGH : u32 = 0x80000000;
+const _ : () = assert!(FLAG_WRITE_THROUGH == winapi::um::winbase::FILE_FLAG_WRITE_THROUGH);
 
 #[doc(alias = "FILE_FLAG_FIRST_PIPE_INSTANCE")]
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createnamedpipea)\]
 /// FILE_FLAG_FIRST_PIPE_INSTANCE
 ///
-pub const FLAG_FIRST_PIPE_INSTANCE : u32 = winapi::um::winbase::FILE_FLAG_FIRST_PIPE_INSTANCE;
+pub const FLAG_FIRST_PIPE_INSTANCE : u32 = 0x00080000;
+const _ : () = assert!(FLAG_FIRST_PIPE_INSTANCE == winapi::um::winbase::FILE_FLAG_FIRST_PIPE_INSTANCE);
 
 #[doc(alias = "FILE_FLAG_OVERLAPPED")]
 /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea)\]
 /// FILE_FLAG_OVERLAPPED
 ///
-pub const FLAG_OVERLAPPED : u32 = winapi::um::winbase::FILE_FLAG_OVERLAPPED;
+pub const FLAG_OVERLAPPED : u32 = 0x40000000;
+const _ : () = assert!(FLAG_OVERLAPPED == winapi::um::winbase::FILE_FLAG_OVERLAPPED);
