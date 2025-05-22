3867716cb55fa8b48e5f4ec4f2ebb010a559085c
MaulingMonkey <git@maulingmonkey.com>
2025-05-21 22:08:03 -0700

    OptionalLibrary: safety docs

--- src/dlls/_dlls.rs
+++ src/dlls/_dlls.rs
@@ -14,6 +14,12 @@ impl OptionalLibrary {
     /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryw)\]
     /// LoadLibraryW
     ///
+    /// ### Safety
+    /// Libraries may have unsound preconditions about the context they're expected to be loaded into,
+    /// and loading said library may immediate execute code (via DllMain, constructors, etc.) which relies on said unsound preconditions.
+    /// Even worse, tricking programs into loading the wrong library as a means of executing malware is a whole thing.
+    /// Use with caution!
+    ///
     pub(crate) unsafe fn load_w(lib_file_name: CStrNonNull<u16>) -> Self {
         let hmodule = unsafe { winapi::um::libloaderapi::LoadLibraryW(
             lib_file_name.as_cstr(),
@@ -23,6 +29,10 @@ impl OptionalLibrary {
 
     /// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress)\]
     /// GetProcAddress
+    ///
+    /// ### Safety
+    /// This assumes the symbol referred to by `proc_name` is a `fn` of type `F`.
+    /// This assumption is utterly unchecked, and could be trivially undermined by anyone wanting to get up to *shennanigans*.
     ///
     pub(crate) unsafe fn get_proc_address<F>(&self, proc_name: &core::ffi::CStr) -> Option<F> {
         use winapi::shared::minwindef::FARPROC;
